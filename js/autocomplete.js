c4a9b6dc2fc3dddb6630881c3665debae34a2019
trehoffman <trehoffman@gmail.com>
2021-05-03 12:30:18 -0400

    Initial commit

---
+++ js/autocomplete.js
@@ -0,0 +1,67 @@
+﻿function Autocomplete(options) {
+    var me = this;
+    me.options = options || {};
+    me.value = "";
+
+    me.init = function () {
+        me.value = me.options.target.value;
+        let id = "datalist_" + Math.floor(Math.random() * 10000).toString() + "_" + Date.now();
+        me.datalist = document.createElement('DATALIST');
+        me.datalist.id = id;
+        document.querySelector('body').appendChild(me.datalist);
+        me.options.target.setAttribute('list', id);
+        me.options.target.setAttribute('autocomplete', 'off');
+
+        if (Array.isArray(me.options.source)) {
+            me.populate(me.options.source);
+        } else if (me.options.prepopulate) {
+            setTimeout(function (value) {
+                if (typeof me.options.source === 'function') me.options.source(value);
+            }, 100, me.value)
+        }
+        
+
+        me.options.target.addEventListener('keyup', function (e) {
+            let value = e.target.value;
+            if (value === me.value) return;
+            me.value = value;
+
+            let minLength = me.options.minLength || 0;
+            if (value.length < minLength) return;
+
+            let delay = me.options.delay || 0;
+            clearTimeout(me.delayTimeout);
+            me.delayTimeout = setTimeout(function (value) {
+                if (typeof me.options.source === 'function') me.options.source(value);
+            }, delay, value);
+        });
+
+        me.options.target.addEventListener('input', function (e) {
+            if (!e.inputType) {
+                let value = e.target.value;
+                me.value = value;
+                let event = new CustomEvent('selected', {
+                    detail: {
+                        value: value
+                    }
+                });
+                me.options.target.dispatchEvent(event);
+            }
+        });
+    };
+
+    me.populate = function (matches) {
+        let innerHTML = '';
+        for (let i = 0; i < matches.length; i++) {
+            let match = matches[i];
+            if (typeof match === 'object') {
+                innerHTML += '<option value="' + match.value + '">' + match.label + '</option>';
+            } else {
+                innerHTML += '<option>' + match + '</option>';
+            }
+        }
+        me.datalist.innerHTML = innerHTML;
+    };
+
+    me.init();
+}
---
+++ js/test.js
@@ -0,0 +1,92 @@
+﻿var autocomplete_1 = new Autocomplete({
+    target: document.querySelector('input[name="test1"]'),
+    source: ["red", "green", "yellow"]
+});
+
+document.querySelector('input[name="test1"]').addEventListener('change', function (e) {
+    let target = e.target;
+    let fieldset = target.closest('fieldset');
+    let div = fieldset.querySelector('div');
+    div.innerText = 'value changed to ' + target.value;
+});
+
+document.querySelector('input[name="test1"]').addEventListener('selected', function (e) {
+    let audio = new Audio('sounds/success.mp3');
+    audio.play();
+});
+
+var autocomplete_2 = new Autocomplete({
+    target: document.querySelector('input[name="test2"]'),
+    source: [{ label: "one", value: "1" }, { label: "two", value: "2" }, { label: "three", value: "3" }]
+});
+
+document.querySelector('input[name="test2"]').addEventListener('change', function (e) {
+    let target = e.target;
+    let fieldset = target.closest('fieldset');
+    let div = fieldset.querySelector('div');
+    div.innerText = 'value changed to ' + target.value;
+});
+
+document.querySelector('input[name="test2"]').addEventListener('selected', function (e) {
+    let audio = new Audio('sounds/success.mp3');
+    audio.play();
+});
+
+var autocomplete_3 = new Autocomplete({
+    target: document.querySelector('input[name="test3"]'),
+    prepopulate: true,
+    source: function (value) {
+        let matches = [];
+        let list = ["Baseball","Basketball","Football"];
+        for (let i = 0; i < list.length; i++) {
+            let x = list[i];
+            if (x.toLowerCase().startsWith(value.toLowerCase())) matches.push(x);
+        }
+        autocomplete_3.populate(matches);
+    }
+});
+
+document.querySelector('input[name="test3"]').addEventListener('change', function (e) {
+    let target = e.target;
+    let fieldset = target.closest('fieldset');
+    let div = fieldset.querySelector('div');
+    div.innerText = 'value changed to ' + target.value;
+});
+
+document.querySelector('input[name="test3"]').addEventListener('selected', function (e) {
+    let audio = new Audio('sounds/success.mp3');
+    audio.play();
+});
+
+var autocomplete_4 = new Autocomplete({
+    target: document.querySelector('input[name="test4"]'),
+    delay: 500,
+    minLength: 1,
+    source: async function (value) {
+        try {
+            let response = await fetch('cities.html');
+            let list = await response.json();
+            let matches = [];
+            for (let i = 0; i < list.length; i++) {
+                let x = list[i];
+                if (x.toLowerCase().startsWith(value.toLowerCase())) matches.push(x);
+            }
+            autocomplete_4.populate(matches);
+        } catch (error) {
+            console.log(error);
+            alert(error);
+        }
+    }
+});
+
+document.querySelector('input[name="test4"]').addEventListener('change', function (e) {
+    let target = e.target;
+    let fieldset = target.closest('fieldset');
+    let div = fieldset.querySelector('div');
+    div.innerText = 'value changed to ' + target.value;
+});
+
+document.querySelector('input[name="test4"]').addEventListener('selected', function (e) {
+    let audio = new Audio('sounds/success.mp3');
+    audio.play();
+});
